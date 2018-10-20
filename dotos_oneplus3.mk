665cc6c9fc33c6b591c150889e0871e365ddc5cf
amit3880 <amit3880@gmail.com>
2018-10-20 06:24:04 +0000

    dotos changes

--- dotos_oneplus3.mk
+++ dotos_oneplus3.mk
@@ -21,7 +21,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/oneplus/oneplus3/device.mk)
 
 # Inherit some common Lineage stuff.
-$(call inherit-product, vendor/arrow/config/common.mk)
+$(call inherit-product, vendor/dot/config/common.mk)
 
 # Vendor security patch level
 # PRODUCT_PROPERTY_OVERRIDES += \
